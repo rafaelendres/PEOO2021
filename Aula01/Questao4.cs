23ce5d0a27c12a7b152a7c1f132e0b0f109a8622
teldebim <teldebim@gmail.com>
2021-01-21 20:17:10 +0000

    Questao 4 concluida

--- Aula01/Questao4.cs
+++ Aula01/Questao4.cs
@@ -8,7 +8,8 @@ class MainClass {
      double a = double.Parse(Console.ReadLine());
      double area = (b*a);
      double per = ((a*2)+(b*2));
-     double m = ((b*b) + (c*c));
+     double m = ((b*b) + (a*a));
+     double diag = Math.Sqrt(m); 
      Console.WriteLine($"AREA={area:0.00} PERIMETRO={per:0.00} DIAGONAL={diag:0.00}");
   }
 }
