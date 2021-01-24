4748c0c843dbc1c7065147416dc3e147700b8e8e
teldebim <teldebim@gmail.com>
2021-01-24 14:06:53 +0000

    Lista repetição em progresso

---
+++ Repeticao/uri1061.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+  
+    Console.WriteLine("Informe a data em que iniciará o evento:");
+    String i = Console.ReadLine();
+
+    Console.WriteLine("Informe a data em que terminará o evento:");
+    String f = Console.ReadLine();
+  
+    }
+
+}
