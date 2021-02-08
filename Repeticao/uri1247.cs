3f45f8af730bb1d37398187f52dbb4ef0635a0e9
teldebim <teldebim@gmail.com>
2021-02-08 15:50:08 +0000

    Uri 1247. A terminar. Não sei como fazer o cálculo.

---
+++ Repeticao/uri1247.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+  int p = 0; 
+  while (p != 'E') 
+  Console.ReadLine();
+  Console.WriteLine("Insira a distância entre uma costa a outra (D), a velocidade do barco do ladrão (VF) e a velocidade do barco da Guarda Costeira(VG)");
+  
+  
+
+  }
+
+}
