c4cb75ccfd4850e88ce134d66813f052fc2d85e0
teldebim <teldebim@gmail.com>
2021-02-21 11:22:04 +0000

    LISTA DE METODOS CONCLUIDA FINALMENTE MEU PAI AMADO

--- Metodos/exe4.cs
+++ Metodos/exe4.cs
@@ -1,3 +1,13 @@
 using System;
   class MainClass {
    public static void Main (){
+     int x = 30;
+     int y = 10; 
+     int w = 0;
+     Console.WriteLine($"MMC = {w}");
+   }
+    public static int MMC(int x, int y){
+    int w = x % 2;
+    return w;
+    }
+  }
--- Metodos/exe5.cs
+++ Metodos/exe5.cs
@@ -1,3 +1,13 @@
 using System;
+  
   class MainClass {
    public static void Main (){
+  
+    string texto = "LA LA LA LA";  
+    Console.WriteLine(texto);
+   }
+   public static string FormatarTexto(string texto) {
+    texto = texto.Trim();
+    return texto;
+   }
+  }
